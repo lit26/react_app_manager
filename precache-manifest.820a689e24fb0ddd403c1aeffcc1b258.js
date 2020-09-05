86e1cc17100ff80ac8bbe6c25d6d16918fbcc482
Tianning Li <ltianningli@gmail.com>
2020-09-04 17:34:33 -0700

    Updates

---
+++ precache-manifest.820a689e24fb0ddd403c1aeffcc1b258.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e12a75a43f39a82a88054caff56662c",
+    "url": "/react_app_manager/index.html"
+  },
+  {
+    "revision": "266e54285349da2feac8",
+    "url": "/react_app_manager/static/css/main.0f273003.chunk.css"
+  },
+  {
+    "revision": "3f0213753e2db3034b54",
+    "url": "/react_app_manager/static/js/2.ad1e54e4.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/react_app_manager/static/js/2.ad1e54e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "266e54285349da2feac8",
+    "url": "/react_app_manager/static/js/main.82de8fea.chunk.js"
+  },
+  {
+    "revision": "bc6e07517f30acb849c2",
+    "url": "/react_app_manager/static/js/runtime-main.dbd1046a.js"
+  }
+]);
